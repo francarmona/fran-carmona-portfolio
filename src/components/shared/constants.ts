0850f902578f3b0c34e45c520a5d68af82113a3c
fcarmona <fcarmona.olmedo@gmail.com>
2024-02-10 16:58:29 +0100

    :sparkles: feat: add about, projects, career & skills

---
+++ src/components/shared/constants.ts
@@ -0,0 +1,49 @@
+import React from '@components/shared/icons/React.astro'
+import Vue from '@components/shared/icons/Vue.astro'
+import NodeJS from '@components/shared/icons/NodeJS.astro'
+import Webpack from '@components/shared/icons/Webpack.astro'
+import TailwindCSS from '@components/shared/icons/TailwindCSS.astro'
+import SWC from '@components/shared/icons/SWC.astro'
+import Sass from '@components/shared/icons/Sass.astro'
+import Redux from '@components/shared/icons/Redux.astro'
+import ReactQuery from '@components/shared/icons/ReactQuery.astro'
+import PHP from '@components/shared/icons/PHP.astro'
+import Laravel from '@components/shared/icons/Laravel.astro'
+import Jest from '@components/shared/icons/Jest.astro'
+import Ionic from '@components/shared/icons/Ionic.astro'
+import Firebase from '@components/shared/icons/Firebase.astro'
+import Docker from '@components/shared/icons/Docker.astro'
+import Cypress from '@components/shared/icons/Cypress.astro'
+import AstroIcon from '@components/shared/icons/Astro.astro'
+import TypeScript from '@components/shared/icons/TypeScript.astro'
+import HTML from '@components/shared/icons/HTML.astro'
+import CSS from '@components/shared/icons/CSS.astro'
+import Figma from '@components/shared/icons/Figma.astro'
+import Angular from '@components/shared/icons/Angular.astro'
+import Javascript from '@components/shared/icons/Javascript.astro'
+
+export const ICONS_MAP = {
+  react: { name: 'React', icon: React },
+  vue: { name: 'Vue', icon: Vue },
+  nodeJS: { name: 'Node.js', icon: NodeJS },
+  tailwindCSS: { name: 'Tailwind CSS', icon: TailwindCSS },
+  typescript: { name: 'TypeScript', icon: TypeScript },
+  jest: { name: 'Jest', icon: Jest },
+  cypress: { name: 'Cypress', icon: Cypress },
+  sass: { name: 'Sass', icon: Sass },
+  html: { name: 'HTML', icon: HTML },
+  css: { name: 'CSS', icon: CSS },
+  reactQuery: { name: 'React Query', icon: ReactQuery },
+  redux: { name: 'Redux', icon: Redux },
+  astro: { name: 'Astro', icon: AstroIcon },
+  webpack: { name: 'Webpack', icon: Webpack },
+  swc: { name: 'SWC', icon: SWC },
+  ionic: { name: 'Ionic', icon: Ionic },
+  firebase: { name: 'Firebase', icon: Firebase },
+  php: { name: 'PHP', icon: PHP },
+  laravel: { name: 'Laravel', icon: Laravel },
+  docker: { name: 'Docker', icon: Docker },
+  figma: { name: 'Figma', icon: Figma },
+  angular: { name: 'Angular', icon: Angular },
+  javascript: { name: 'Javascript', icon: Javascript }
+}
