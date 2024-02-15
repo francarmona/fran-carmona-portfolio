e83eaa603a4d52c9d595d62918e2736893e6d62e
fcarmona <fcarmona.olmedo@gmail.com>
2024-02-15 22:08:02 +0100

    :recycle: refactor: create Icon component for svgs

--- src/components/shared/constants.ts
+++ src/components/shared/constants.ts
@@ -1,53 +1,27 @@
-import React from '@components/shared/icons/React.astro'
-import Vue from '@components/shared/icons/Vue.astro'
-import NodeJS from '@components/shared/icons/NodeJS.astro'
-import Webpack from '@components/shared/icons/Webpack.astro'
-import TailwindCSS from '@components/shared/icons/TailwindCSS.astro'
-import SWC from '@components/shared/icons/SWC.astro'
-import Sass from '@components/shared/icons/Sass.astro'
-import Redux from '@components/shared/icons/Redux.astro'
-import ReactQuery from '@components/shared/icons/ReactQuery.astro'
-import PHP from '@components/shared/icons/PHP.astro'
-import Laravel from '@components/shared/icons/Laravel.astro'
-import Jest from '@components/shared/icons/Jest.astro'
-import Ionic from '@components/shared/icons/Ionic.astro'
-import Firebase from '@components/shared/icons/Firebase.astro'
-import Docker from '@components/shared/icons/Docker.astro'
-import Cypress from '@components/shared/icons/Cypress.astro'
-import AstroIcon from '@components/shared/icons/AstroIcon.astro'
-import TypeScript from '@components/shared/icons/TypeScript.astro'
-import HTML from '@components/shared/icons/HTML.astro'
-import CSS from '@components/shared/icons/CSS.astro'
-import Figma from '@components/shared/icons/Figma.astro'
-import Angular from '@components/shared/icons/Angular.astro'
-import Javascript from '@components/shared/icons/Javascript.astro'
-import ReactTestingLibrary from '@components/shared/icons/ReactTestingLibrary.astro'
-import MaterialUI from '@components/shared/icons/MaterialUI.astro'
-
 export const ICONS_MAP = {
-  react: { name: 'React', icon: React },
-  vue: { name: 'Vue', icon: Vue },
-  nodeJS: { name: 'Node.js', icon: NodeJS },
-  tailwindCSS: { name: 'Tailwind CSS', icon: TailwindCSS },
-  typescript: { name: 'TypeScript', icon: TypeScript },
-  jest: { name: 'Jest', icon: Jest },
-  cypress: { name: 'Cypress', icon: Cypress },
-  sass: { name: 'Sass', icon: Sass },
-  html: { name: 'HTML', icon: HTML },
-  css: { name: 'CSS', icon: CSS },
-  reactQuery: { name: 'React Query', icon: ReactQuery },
-  redux: { name: 'Redux', icon: Redux },
-  astro: { name: 'Astro', icon: AstroIcon },
-  webpack: { name: 'Webpack', icon: Webpack },
-  swc: { name: 'SWC', icon: SWC },
-  ionic: { name: 'Ionic', icon: Ionic },
-  firebase: { name: 'Firebase', icon: Firebase },
-  php: { name: 'PHP', icon: PHP },
-  laravel: { name: 'Laravel', icon: Laravel },
-  docker: { name: 'Docker', icon: Docker },
-  figma: { name: 'Figma', icon: Figma },
-  angular: { name: 'Angular', icon: Angular },
-  javascript: { name: 'Javascript', icon: Javascript },
-  reactTestingLibrary: { name: 'RTL', icon: ReactTestingLibrary },
-  materialUI: { name: 'Material UI', icon: MaterialUI }
+  react: { name: 'React', icon: 'react' },
+  vue: { name: 'Vue', icon: 'vue' },
+  nodeJS: { name: 'Node.js', icon: 'node-js' },
+  tailwindCSS: { name: 'Tailwind CSS', icon: 'tailwind-css' },
+  typescript: { name: 'TypeScript', icon: 'typescript' },
+  jest: { name: 'Jest', icon: 'jest' },
+  cypress: { name: 'Cypress', icon: 'cypress' },
+  sass: { name: 'Sass', icon: 'sass' },
+  html: { name: 'HTML', icon: 'html' },
+  css: { name: 'CSS', icon: 'css' },
+  reactQuery: { name: 'React Query', icon: 'react-query' },
+  redux: { name: 'Redux', icon: 'redux' },
+  astro: { name: 'Astro', icon: 'astro' },
+  webpack: { name: 'Webpack', icon: 'webpack' },
+  swc: { name: 'SWC', icon: 'swc' },
+  ionic: { name: 'Ionic', icon: 'ionic' },
+  firebase: { name: 'Firebase', icon: 'firebase' },
+  php: { name: 'PHP', icon: 'php' },
+  laravel: { name: 'Laravel', icon: 'laravel' },
+  docker: { name: 'Docker', icon: 'docker' },
+  figma: { name: 'Figma', icon: 'figma' },
+  angular: { name: 'Angular', icon: 'angular' },
+  javascript: { name: 'Javascript', icon: 'javascript' },
+  reactTestingLibrary: { name: 'RTL', icon: 'react-testing-library' },
+  materialUI: { name: 'Material UI', icon: 'material-ui' }
 }
--- src/components/shared/icons/constants.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_ICON_CLASS = 'h-5 w-5'
